e5c99cc0e548f12527b7c3f9fecb793eaf5face9
iamstorm0530 <23162105@student.hcmute.edu.vn>
2025-08-22 23:57:05 +0700

    Update User.java

--- src/main/java/murach/business/User.java
+++ src/main/java/murach/business/User.java
@@ -7,24 +7,20 @@ public class User implements Serializable {
     private String lastName;
     private String email;
 
-    public User() {
-        firstName = "";
-        lastName = "";
-        email = "";
-    }
+    public User() {}
 
     public User(String firstName, String lastName, String email) {
         this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
+        this.lastName  = lastName;
+        this.email     = email;
     }
 
     public String getFirstName() { return firstName; }
-    public void setFirstName(String firstName) { this.firstName = firstName; }
+    public void setFirstName(String v) { firstName = v; }
 
     public String getLastName() { return lastName; }
-    public void setLastName(String lastName) { this.lastName = lastName; }
+    public void setLastName(String v) { lastName = v; }
 
     public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public void setEmail(String v) { email = v; }
 }
