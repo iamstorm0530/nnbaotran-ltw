80104d58b6cb09fa0ea28e93ea6a4bbb1be6e83c
iamstorm0530 <23162105@student.hcmute.edu.vn>
2025-08-22 23:57:29 +0700

    Update UserDB.java

--- src/main/java/murach/data/UserDB.java
+++ src/main/java/murach/data/UserDB.java
@@ -1,20 +1,8 @@
 package murach.data;
 
-import java.util.ArrayList;
-import java.util.List;
 import murach.business.User;
 
 public class UserDB {
-    private static final List<User> USERS = new ArrayList<>();
-
-    public static void insert(User user) {
-        if (user != null) {
-            USERS.add(user);
-            System.out.println("Inserted user: " + user.getEmail());
-        }
-    }
-
-    public static List<User> selectAll() {
-        return new ArrayList<>(USERS);
-    }
+    // Demo: không lưu DB thật, chỉ để code compile/chạy
+    public static void insert(User user) { /* no-op */ }
 }
